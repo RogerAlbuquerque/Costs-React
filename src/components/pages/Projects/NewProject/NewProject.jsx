d37b900ce13688f02bd6ceba855ae547ed2ce04e
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-07-08 18:39:34 -0300

    Criado e iniciado as configurações da pagina de projetos

--- src/components/pages/Projects/NewProject/NewProject.jsx
+++ src/components/pages/Projects/NewProject/NewProject.jsx
@@ -1,16 +1,34 @@
 import './NewProject.css'
+import Button from '../../../extraComponents/Buttons'
 export default function NewProject(){
     return(
         <article className="NewProject">
             <section>
                 <h1>Criar projeto</h1>
                 <p>Crie seus projetos para depois adicionar os serviços</p>
+
+                <form action="">
+                    <label htmlFor="">Nome do projeto:</label>
+                        <input type="text" />
+                    
+                    <label htmlFor="">Orçamento do projeto:</label>
+                        <input type="number" name="" />
+                    
+                    <label htmlFor="">Selecione a categoría:</label>
+                        <select name="" id="">
+                            <option disabled>Selecione uma opção</option>
+                            <option value="">Primeira</option>
+                            <option value="">Segunda</option>
+                        </select>
+
+                        <Button />
+                    
+
+                </form>
             </section>
-           
-           
-                 <h1>FORMULÁRIO</h1>
-            <section>
-            </section>
+            
+                
+        
         </article>
     )
 }
