f43712d44a9e678d378af85fbc43366e6b8064d4
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-08-27 17:08:39 -0300

    tentando deploy de novo

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// "https://RogerAlbuquerque.github.io/Costs-React/"
 export default defineConfig({
   plugins: [react()]
 })
