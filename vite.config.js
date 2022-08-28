7fd853fd41849096451f6859e87707dc21d66a3c
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-08-27 22:58:43 -0300

    ssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // "https://RogerAlbuquerque.github.io/Costs-React/"
 export default defineConfig({
-  base:"https://rogeralbuquerque.github.io/",
+  base:"https://rogeralbuquerque.github.io/Costs-React",
   plugins: [react()]
 })
