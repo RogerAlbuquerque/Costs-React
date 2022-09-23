349f94e11d80694bd165f8334586dcd41b0e0438
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2022-09-23 16:14:59 -0300

    ADICIONANDO LLINKS PAARA CONTATOS NA PAGINA SOBRE

--- src/components/pages/randons/Contact.jsx
+++ src/components/pages/randons/Contact.jsx
@@ -16,8 +16,12 @@ export default function Contact(){
                 </p>
 
                 <p style={{color: "white", padding:"1rem 1rem", backgroundColor:"#B10000" }}>
-                    Enquanto o projeto não é finalizado, fique a vontade para ver o meu trabalho desenvolvendo esse projeto, foi o projeto mais complicado que fiz desde que iniciei no frontend, todo o codigo está disponível no meu perfil do github juntamente com os códigos para fazer a implantação da API com o backend:
-                    <a style={{color:"#0FB1AE"}} href="https://github.com/RogerAlbuquerque/Costs-React" target="_blank"> https://github.com/RogerAlbuquerque/Costs-React</a>
+                    Enquanto o projeto não é finalizado, fique a vontade para ver o meu trabalho desenvolvendo esse projeto, ele foi o projeto mais complicado que fiz desde que iniciei no frontend, todo o codigo está disponível no meu perfil do github juntamente com os códigos para fazer a implantação da API com o backend:
+                    <a style={{color:"#0FB1AE"}} href="https://github.com/RogerAlbuquerque/Costs-React" target="_blank"> GITHUB </a><br /><br />
+
+                    Me acompananhe no linkedin também <a href="https://www.linkedin.com/in/roger-albuquerque/">LINKEDIN</a> e no <a href="https://www.instagram.com/estudahack/">INSTAGRAM</a>
+
+                
                     
                 </p>
             </section>
